fe77a1a7510e8bc57375f8f5dfc4129851883c86
ffppa <fabio.felici@pagopa.it>
2026-09-18 17:11:11 +0200

    feat: Disable export of keys in Cloudo AI Agent configuration for enhanced security

--- src/core/iac/main.tf
+++ src/core/iac/main.tf
@@ -101,7 +101,7 @@ module "cloudo_agent" {
   storage_account_name       = module.storage_account.name
   storage_account_access_key = module.storage_account.primary_access_key
   app_service_plan_name      = "${var.prefix}-cloudo-agent-service-plan"
-  export_keys                = true
+  export_keys                = false
 
   app_settings = {
     "AI_ANALYSIS_QUEUE_NAME"              = azurerm_storage_queue.ai_analysis[0].name
